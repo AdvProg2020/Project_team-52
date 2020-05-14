a9deca6c66920e0318b7df2d23f818b0b1777338
parimehrmorassa <62252489+parimehrmorassa@users.noreply.github.com>
2020-05-14 14:04:14 +0430

    parimehr

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,12 @@
+package Model;
+
+import controller.account.Account;
+import exception.NotEnoughCreditException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
+public class User {
+}
